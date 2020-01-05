4d3782d692fbb3ba4fa945ce0c3829ba094130da
Boris Tashkulov <boris.tashkulov@gmail.com>
2020-01-05 18:32:47 +0400

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  # how to create one validates record for many fields?
+  validates :first_name, length: { minimum: 2}
+  validates :last_name, length: { minimum: 2 }
+  validates :email, uniqueness: { case_sensitive: false }, 
+    format: { with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i, multiline: true }
 end
